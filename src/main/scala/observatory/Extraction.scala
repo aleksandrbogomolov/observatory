40cb5a2ed9675ffecb63149857be296e692f0b00
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2018-02-11 18:20:09 +0300

    Solution with RDD, don't passed

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,12 +1,10 @@
 package observatory
 
 import java.nio.file.Paths
-import java.sql.Date
 import java.time.LocalDate
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 /**
@@ -14,9 +12,7 @@ import org.apache.spark.sql.{Row, SparkSession}
   */
 object Extraction {
 
-  val spark: SparkSession = SparkSession.builder().appName("extraction").config("spark.master", "local").getOrCreate()
-
-  import spark.implicits._
+  val sc: SparkContext = new SparkContext("local", "extraction", new SparkConf())
 
   /**
     * @param year             Year number
@@ -25,61 +21,46 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    val stationRDD: RDD[Row] = spark.sparkContext.textFile(getPath(stationsFile))
+    val stationRDD: RDD[(String, (String, String))] = sc.textFile(getPath(stationsFile))
       .map(_.split(",").to[List])
       .filter(_.lengthCompare(4) == 0)
       .filter(l => !l.head.isEmpty || !l(1).isEmpty || !l(2).isEmpty || !l(3).isEmpty)
       .map(stationRow)
-    val stationDF = spark.createDataFrame(stationRDD, stationSchema)
 
-    val year = getYear(temperaturesFile)
-    val temperatureRDD: RDD[Row] = spark.sparkContext.textFile(getPath(temperaturesFile))
+    val temperatureRDD: RDD[(String, (String, String, String, String))] = sc.textFile(getPath(temperaturesFile))
       .map(_.split(",").to[List])
       .filter(_.lengthCompare(5) == 0)
+      .filter(l => !l.head.isEmpty && !l(1).isEmpty)
+      .filter(l => l.last.toDouble != 9999.9)
       .map(temperatureRow(_, year))
-    val temperatureDF = spark.createDataFrame(temperatureRDD, temperatureSchema)
 
-    val joinedDF = stationDF.join(temperatureDF, "id")
-
-    val result = joinedDF.select("*").map(r =>
+    stationRDD.join(temperatureRDD).map(p =>
       (
-        Date.valueOf(s"${r.get(3)}-${r.get(4)}-${r.get(5)}"),
-        Location(r.get(1).toString.toDouble, r.get(2).toString.toDouble),
-        getCelsius(r.get(6).toString.toDouble)
+        LocalDate.parse(s"${p._2._2._1}-${p._2._2._2}-${p._2._2._3}"),
+        Location(p._2._1._1.toDouble, p._2._1._2.toDouble),
+        getCelsius(p._2._2._4.toDouble)
       )
     ).collect()
-
-    result.map(t => (t._1.toLocalDate, t._2, t._3))
   }
 
   private def getPath(localFile: String) = Paths.get(getClass.getResource(localFile).toURI).toString
 
-  private def getYear(str: String): Year = {
-    val pattern = "\\d+".r
-    pattern.findFirstIn(str).get.toInt
-  }
-
   private def getCelsius(fahrenheit: Double): Temperature = (fahrenheit - 32) / 1.8
 
-  private def stationRow(line: List[String]): Row = Row(line.head + line(1), line(2), line(3))
+  private def stationRow(line: List[String]): (String, (String, String)) = (line.head + line(1), (line(2), line(3)))
 
-  private def stationSchema: StructType = StructType("id latitude longitude".split(" ").map(s => StructField(s,
-    StringType, nullable = false)))
-
-  private def temperatureRow(line: List[String], year: Year): Row = Row(line.head + line(1), year.toString, line(2), line(3),
-    line.last)
-
-  private def temperatureSchema: StructType = StructType("id year month day temperature".split(" ")
-    .map(s => StructField(s, StringType, nullable = false)))
+  private def temperatureRow(line: List[String], year: Year): (String, (String, String, String, String)) = (line.head +
+    line(1), (year.toString, line(2), line(3), line.last))
 
   /**
     * @param records A sequence containing triplets (date, location, temperature)
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    records.map(t => (t._2, t._3)).groupBy(_._1)
-      .map { case (k, v) =>
-        (k, v.map(_._2).sum / v.size)
-      }
+    records.par
+      .groupBy(_._2)
+      .mapValues(t =>
+        t.foldLeft(0.0)((sum, t) => sum + t._3) / t.size)
+      .seq
   }
 }
